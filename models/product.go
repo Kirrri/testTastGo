fcbf9e48e52ab9498f4ba361fdd5173cf5d60d0d
Kirrri <kazantsev_kiril@bk.ru>
2024-07-26 12:08:23 +0700

    работа над ошибками

--- models/product.go
+++ models/product.go
@@ -4,6 +4,6 @@ type Product struct {
 	Id        int    `json:"id"`
 	Name      string `json:"name"`
 	Quantity  int    `json:"quantity"`
-	UnitCoast int64  `json:"unit_coast"`
+	UnitCost  int64  `json:"unit_cost"`
 	MeasureID int    `json:"measure_id"`
 }
